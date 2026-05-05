e0f6c703c0aebdd36fd734ddb7537ee562798f6d
Prasanth <prasanth530001@gmail.com>
2026-05-05 17:51:22 +0530

    initial commit

---
+++ src/main/java/com/gotomock/GoToMockApplication.java
@@ -0,0 +1,13 @@
+package com.gotomock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoToMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoToMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gotomock/GotomockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotomock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotomockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
