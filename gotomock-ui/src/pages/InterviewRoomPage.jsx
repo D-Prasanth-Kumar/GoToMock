9321b75b7223c5f96abb4dc03d15bb4d3b6f6f68
Prasanth <prasanth530001@gmail.com>
2026-07-02 14:18:13 +0530

    fix interview page

--- gotomock-ui/src/pages/InterviewRoomPage.jsx
+++ gotomock-ui/src/pages/InterviewRoomPage.jsx
@@ -215,7 +215,26 @@ function InterviewRoomPage() {
         if (peerConnectionRef.current) return;
 
         const peerConnection = new RTCPeerConnection({
-            iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+            iceServers: [
+                { urls: "stun:stun.l.google.com:19302" },
+                { urls: "stun:stun1.l.google.com:19302" },
+                { urls: "stun:openrelay.metered.ca:80" },
+                {
+                    urls: "turn:openrelay.metered.ca:80",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                }
+            ]
         });
 
         peerConnection.onicecandidate = (event) => {
