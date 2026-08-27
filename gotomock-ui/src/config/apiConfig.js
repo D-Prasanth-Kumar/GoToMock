06d15cf000e5622bc18661f35d265d6fbd2ce63f
Prasanth <prasanth530001@gmail.com>
2026-08-28 00:10:27 +0530

    some fix

--- gotomock-ui/src/config/apiConfig.js
+++ gotomock-ui/src/config/apiConfig.js
@@ -1,2 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
-export const WS_BASE_URL = import.meta.env.VITE_WS_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://gotomock-backend.onrender.com";
+const WS_URL = import.meta.env.VITE_WS_URL || "wss://gotomock-backend.onrender.com";
+
+export const API_BASE_URL = API_URL;
+export const WS_BASE_URL = WS_URL;
